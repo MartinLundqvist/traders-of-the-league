ff5d4a3235112b811d5acde25dbda57ccf6ad662
MartinLundqvist <iphonelynden@gmail.com>
2022-04-24 20:24:32 +0200

    Removed unused shared functions

--- shared/utils/randomPoint.ts
+++
@@ -1,6 +0,0 @@
-import { TVictoryPoint } from '../types';
-
-export const randomPoint = (): TVictoryPoint => {
-  // min and max included
-  return Math.floor(Math.random() * 5 + 1) as TVictoryPoint;
-};
