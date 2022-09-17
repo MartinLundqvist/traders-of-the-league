a1c878857736605930d7b9ce2e15dc7c6ddb470b
MartinLundqvist <iphonelynden@gmail.com>
2022-09-17 16:57:30 +0200

    Fixed fonts again...

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,9 @@ const path = require('path');
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
