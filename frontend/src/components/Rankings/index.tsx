94fd1db53604f2d0659936a023a68e4954b39b7b
MartinLundqvist <iphonelynden@gmail.com>
2022-11-19 19:33:49 +0100

    Scrollable ranking table

--- frontend/src/components/Rankings/index.tsx
+++ frontend/src/components/Rankings/index.tsx
@@ -10,12 +10,24 @@ const URL = import.meta.env.VITE_URL;
 const Container = styled.div`
   /* border: 2px solid red; */
   width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  gap: 2rem;
+  justify-content: center;
+  align-items: center;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  gap: 0.5rem;
+  overflow: hidden;
 
   .text-centered {
     text-align: center;
+  }
+
+  .divider {
+    height: 1px;
+    width: 100%;
+    background-color: var(--color-background-dark);
   }
 
   .padded {
@@ -23,16 +35,31 @@ const Container = styled.div`
     padding-bottom: 2rem;
   }
 
-  .margin-centered {
-    margin: auto;
-  }
+  .table-container {
+    position: relative;
+    width: 100%;
+    height: 65%;
+    overflow-y: scroll;
 
-  table {
-    font-size: 1.2rem;
-    text-align: left;
+    table {
+      position: relative;
+      width: 100%;
+      font-size: 1.2rem;
+      text-align: left;
 
-    th {
-      font-size: 1.5rem;
+      th {
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+        text-align: left;
+
+        font-size: 1.5rem;
+        border: none;
+      }
+
+      td {
+        border: none;
+        padding: 0.5rem;
+      }
     }
   }
 `;
@@ -76,44 +103,47 @@ const Rankings = ({ className }: IRankingsProps): JSX.Element => {
     <ScrollFull landscape className={className}>
       <Container>
         <Title className='text-centered'>Rankings</Title>
-        <table>
-          <thead>
-            <tr>
-              <th>Player</th>
-              <th>Games</th>
-              <th>Ranking</th>
-            </tr>
-          </thead>
-          <tbody>
-            {ranked?.map((ranking, index) => (
-              <tr key={ranking.user.uuid}>
-                <td>{ranking.user.name}</td>
-                <td>{ranking.rankingHistory.length}</td>
-                <td>{ranking.currentRanking}</td>
+        <div className='divider' />
+        <div className='table-container'>
+          <table>
+            <thead>
+              <tr>
+                <th>Player</th>
+                <th>Games</th>
+                <th>Ranking</th>
               </tr>
-            ))}
-            <tr>
-              <td className='text-centered padded' colSpan={3}>
-                Unranked players:
-              </td>
-            </tr>
-            {unRanked?.map((ranking, index) => (
-              <tr key={ranking.user.uuid}>
-                <td>{ranking.user.name}</td>
-                <td>{ranking.rankingHistory.length}</td>
-                <td>N/A</td>
+            </thead>
+            <tbody>
+              {ranked?.map((ranking, index) => (
+                <tr key={ranking.user.uuid}>
+                  <td>{ranking.user.name}</td>
+                  <td>{ranking.rankingHistory.length}</td>
+                  <td>{ranking.currentRanking}</td>
+                </tr>
+              ))}
+              <tr>
+                <td className='text-centered padded' colSpan={3}>
+                  Unranked players:
+                </td>
               </tr>
-            ))}
-          </tbody>
-        </table>
+              {unRanked?.map((ranking, index) => (
+                <tr key={ranking.user.uuid}>
+                  <td>{ranking.user.name}</td>
+                  <td>{ranking.rankingHistory.length}</td>
+                  <td>N/A</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className='divider' />
         <div className='text-centered'>
           You need minimum 5 ranked games to appear on the list
         </div>
-        <div className='margin-centered'>
-          <ButtonSmall onClick={() => setActiveRoute('start')}>
-            Back to menu
-          </ButtonSmall>
-        </div>
+
+        <ButtonSmall onClick={() => setActiveRoute('start')}>
+          Back to menu
+        </ButtonSmall>
       </Container>
     </ScrollFull>
   );
